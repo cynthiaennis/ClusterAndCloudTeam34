cc4e7a298b70567935429b3a3b6bacb9aaedc82b
EthanChenYen-Peng <ypcethan@gmail.com>
2019-04-22 15:24:43 +1000

    Adding the map page

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-
+import MapWithAMarker from './MapComponent'
 export default class MapPage extends Component {
   render() {
     return (
       <div>
-        Map
+        <MapWithAMarker
+          containerElement={<div style={{ height: `500px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
     )
   }
