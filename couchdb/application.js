59ba67df66e64e4efa275cd81f0d4d4b39bb3805
cyn <45725475+cyn@users.noreply.github.com>
2019-05-11 12:14:53 +1000

    use correct field names

--- couchdb/application.js
+++ couchdb/application.js
@@ -2,11 +2,11 @@
     "language": "javascript",
     "views": {
         "location_count_positive": {
-            "map": "function(doc) { if ( doc.user.location && doc.positive) { if (doc.positive == true) { emit(doc.user.location, 1); } } }",
+            "map": "function(doc) { if ( doc.tweet.tweet_location && doc.tweet.is_negative_sentiment) { if (doc.tweet.is_negative_sentiment == 1) { emit(doc.tweet.tweet_location, 1); } } }",
             "reduce":"_count"
         },
 	"location_count_all": {
-            "map": "function(doc) { if ( doc.user.location ){ emit(doc.user.location, 1); } }",
+            "map": "function(doc) { if ( doc.tweet.tweet_location ){ emit(doc.tweet.tweet_location, 1); } }",
 	    "reduce":"_count"
         }
     }
