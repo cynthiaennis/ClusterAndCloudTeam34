832e03ead7cea9325efe631f7946c6f498d9aa63
cyn <45725475+cyn@users.noreply.github.com>
2019-05-12 14:43:49 +1000

    change database name and field names

--- couchdb/application.js
+++ couchdb/application.js
@@ -2,7 +2,7 @@
     "language": "javascript",
     "views": {
         "location_count_positive": {
-            "map": "function(doc) { if ( doc.tweet.tweet_location && doc.tweet.is_negative_sentiment) { if (doc.tweet.is_negative_sentiment == 1) { emit(doc.tweet.tweet_location, 1); } } }",
+            "map": "function(doc) { if ( doc.tweet.tweet_location && doc.tweet.is_gegative_sentiment) { if (doc.tweet.is_gegative_sentiment == 1) { emit(doc.tweet.tweet_location, 1); } } }",
             "reduce":"_count"
         },
 	"location_count_all": {
